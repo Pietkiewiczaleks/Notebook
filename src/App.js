2d9abcf7ad441561b1239d926157fd1e08914e86
Pietkiewiczaleks <89607989+Pietkiewiczaleks@users.noreply.github.com>
2022-03-14 18:20:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { render } from "@testing-library/react";
+import React, {Component} from "react";
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newNote:"",
+      list:[]
+    }
+  }
+  updateInput(key, value) {
+    this.setState({
+      [key]: value
+    })
+  }
+  addNote() {
+    const newNote={
+      id: 1 + Math.random(),
+      value: this.state.newNote.slice()
+    };
+    const list = [...this.state.list];
+    list.push(newNote);
+    this.setState({
+      list,
+      newNote:""
+    });
+
+  };
+    deleteNote(id){
+      const list = [...this.state.list];
+      const updatedList = list.filter(note => note.id !== id);
+      this.setState({list: updatedList});
+  }
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App" class="container">
+      <div class="article">
+        <p class="header">Note</p>
+        <input type="text"
+          placeholder="Note text"
+          value={this.state.newNote}
+          onChange={e => this.updateInput("newNote", e.target.value)}
+         class="main-text-input"
+        />
+      
+        <button
+         onClick={() => this.addNote()}
+         class="btn-primary"
         >
-          Learn React
-        </a>
-      </header>
+          Add note
+        </button>
+        <h2>Latest notes</h2>
+        <ul>
+          {this.state.list.map(note =>{
+            return(
+              <li key={note.id}
+              class="li-el">
+                <div class="note-section">
+                <div class="note-content">{note.value}</div>
+                <button
+                 onClick={() => this.deleteNote(note.id)}
+                 class="btn"
+                >
+                  Delete note
+                </button>
+                </div>
+              </li>
+          )
+        })}
+      </ul>
+      </div>
     </div>
   );
 }
+} 
 
 export default App;
